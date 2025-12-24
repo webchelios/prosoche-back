b69dadbe5dc6dfd9e6766b76cb5f160256af610f
marcelo anavia <marcelo.anavia@gmail.com>
2025-12-24 02:05:19 -0300

    note usecases implementation

--- src/presentation/notes/notes-controller.ts
+++ src/presentation/notes/notes-controller.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
-import { NotesRepository } from "../../domain";
-
-
-const notes = [{ id: 1, title: 'Titulo', content: 'hola', createdAt: new Date() }, { id: 2, title: 'Titulo2', content: 'adios', createdAt: new Date() }]
+import { CreateNote, GetNote, GetNotes, NotesRepository } from "../../domain";
 
 export class NotesController {
     constructor(
@@ -11,22 +8,19 @@ export class NotesController {
     ) { }
 
     getNotes = (req: Request, res: Response) => {
-        this.notesRepository.getAll()
+        new GetNotes(this.notesRepository)
+            .execute()
+            .then(notes => res.json(notes))
+            .catch(error => res.status(400).json({ error }))
     }
 
     getNoteById = (req: Request, res: Response) => {
-        const id = +req.params.id
-        if (isNaN(id)) return res.status(400).json({ error: `Argument ID is not a number` })
+        const id = req.params.id
 
-        const note = notes.find((note) => {
-            return note.id === id
-        })
-        if (note) {
-            res.status(200).json(note)
-        } else {
-            res.status(404).json(`Note with ID: ${id} not found`)
-        }
-
+        new GetNote(this.notesRepository)
+            .execute(id)
+            .then(note => res.json(note))
+            .catch(error => res.status(400).json({ error }))
 
     }
 
@@ -35,53 +29,20 @@ export class NotesController {
         if (!title) return res.status(400).json({ error: `Title is required` })
         if (!content) return res.status(400).json({ error: `Content is required` })
 
-        const newNote = {
-            id: notes.length + 1,
-            title,
-            content,
-            createdAt: new Date()
-        }
+        new CreateNote(title)
 
-        notes.push(newNote)
-
-        res.json(newNote)
     }
 
     updateNote = (req: Request, res: Response) => {
         const id = +req.params.id
 
 
-        const note = notes.find((note) => {
-            return note.id === id
-        })
-        if (!note) {
-            return res.status(404).json(`Note with ID: ${id} not found`)
-        }
 
-        const { title, content, createdAt } = req.body
-        if (!title || !content || !createdAt) return res.status(400).json({ error: `Title, content or createdAt are required` })
-
-        note.title = title || note.title
-        note.content = content || note.content
-        note.createdAt = new Date(createdAt || note.createdAt)
-
-
-        res.json(note)
     }
 
     deleteNote = (req: Request, res: Response) => {
         const id = +req.params.id
 
 
-        const note = notes.find((note) => {
-            return note.id === id
-        })
-        if (!note) {
-            return res.status(404).json(`Note with ID: ${id} not found`)
-        }
-
-        notes.splice(notes.indexOf(note), 1)
-
-        res.json(notes)
     }
 }
